e7bfcc5a440ffb663ac15cc3e5c7a224c88d7b69
luketevl <luketevl@gmail.com>
2017-12-08 17:26:56 -0200

    initial commit

---
+++ udemy/js-com-tdd-na-pratica/myproject/src/testlint.js
@@ -0,0 +1,3 @@
+a = 3
+const c = "ahfiuhiuhiuehgiuehgurieghieruhgeiurhgiurehgiurehgiurhgiuerhgiruehguierhgireuhgieughiueghriueghieughruigheiughruigheuihgiurehgieurhgeuihgieughueirghiuergheuihghguerihguirehguieruhgiuehgie"
+console.log(a, b);
